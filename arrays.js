63090409671e58d12add5288bb60c80c37641346
Matt Readout <matt.readout@gmail.com>
2018-08-27 21:06:55 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -6,7 +6,7 @@ var chocolateBars = [
   ]
   
   function addElementToBeginningOfArray(array, element) {
-    
+    ['${element}'...array]
   }
   
   function destructivelyAddElementToBeginningOfArray(array, element) {
